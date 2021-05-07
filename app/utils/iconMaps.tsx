09abfa6a7b4370cce9f96ccba7347b8c951f11c8
Marian Kmotorka <Marian.Kmotorka@quest.com>
2021-05-07 22:08:55 +0200

    Revert "better typing"

--- app/utils/iconMaps.tsx
+++ app/utils/iconMaps.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react'
 import { Group, Lock, Public } from '@material-ui/icons'
 import { AccessibilityTypeEnum } from 'domainTypes'
 
-export const accessibilityTypeIconMap: Record<AccessibilityTypeEnum, ReactNode> = {
+export const accessibilityTypeIconMap: Record<number, ReactNode> = {
   [AccessibilityTypeEnum.Public]: <Public />,
   [AccessibilityTypeEnum.Private]: <Lock />,
   [AccessibilityTypeEnum.FriendsOnly]: <Group />
