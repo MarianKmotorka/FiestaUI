4d557141a6c86a5843267805abdb75cc79b662b5
Marian Kmotorka <Marian.Kmotorka@quest.com>
2021-05-07 21:38:25 +0200

    Better type for accessibilityTypeIconMap

--- app/utils/iconMaps.tsx
+++ app/utils/iconMaps.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react'
 import { Group, Lock, Public } from '@material-ui/icons'
 import { AccessibilityTypeEnum } from 'domainTypes'
 
-export const accessibilityTypeIconMap: Record<number, ReactNode> = {
+export const accessibilityTypeIconMap: Record<AccessibilityTypeEnum, ReactNode> = {
   [AccessibilityTypeEnum.Public]: <Public />,
   [AccessibilityTypeEnum.Private]: <Lock />,
   [AccessibilityTypeEnum.FriendsOnly]: <Group />
