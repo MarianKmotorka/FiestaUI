f18167e97b3d34e7f0ea730686af3b32397a8902
Marian Kmotorka <Marian.Kmotorka@quest.com>
2021-08-27 17:45:54 +0200

    next config test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const webpack = require('webpack')
 const nextTranslate = require('next-translate')
 
+console.log('Next.config.js file detected')
+
 module.exports = nextTranslate({
   webpack: config => {
     config.node = {
@@ -20,5 +22,6 @@ module.exports = nextTranslate({
   },
   images: {
     domains: ['res.cloudinary.com', 'lh4.googleusercontent.com', 'lh3.googleusercontent.com']
-  }
+  },
+  target: 'serverless'
 })
